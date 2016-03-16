9bf97dc29e77d1c1358f99fea0208361d77c33b1
Bernard Teske <info@bernardteske.de>
2016-03-16 16:15:58 +0100

    Added a method for create and a method for adding Data

--- btparsecsv.lib.php
+++ btparsecsv.lib.php
@@ -33,6 +33,54 @@ class btparseCSV {
     return $array;
   }
   
+  public function create($array, $filename)
+  {
+    $answer=array();
+    if(!is_file($filename)){      
+      
+      $keys=array_keys($array);
+      
+      $fp = fopen($filename, 'w');
+      fputcsv($fp, $keys, ';', "'");
+      fclose($fp);
+      
+      $answer['status']='1';
+      $answer['error']='';
+    }
+    else{
+      $answer['status']='0';
+      $answer['error']='File was not created! Already exists!';
+    }
+    return $answer;
+  }
+  
+  public function add($array, $filename)
+  {
+    $answer=array();
+    if(!is_file($filename.'.locked')){
+      copy($filename, $filename.'.locked');
+      if(!is_file($filename.'.backup')){
+        copy($filename, $filename.'.backup');
+      }
+      else{
+        @unlink($filename.'.backup');
+        copy($filename, $filename.'.backup');
+      }
+      
+      $fp = fopen($filename, 'a');
+      fputcsv($fp, $array, ';');
+      fclose($fp);
+      unlink($filename.'.locked');
+      $answer['status']='1';
+      $answer['error']='';
+    }
+    else{
+      $answer['status']='0';
+      $answer['error']='File locked';
+    }
+    return $answer;
+  }
+  
   public function save($array, $filename)
   {
     $answer=array();
